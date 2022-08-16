01a068799d0bf7458b86a890d91955328a0f81c5
YinggeSun <yingges@student.unimelb.edu.au>
2022-08-17 00:39:07 +0800

    Yingge Had try and run the code successfully!

--- medione/src/main/java/com/example/Medione/Team10Application.java
+++ medione/src/main/java/com/example/Medione/Team10Application.java
@@ -2,7 +2,7 @@ package com.example.Medione;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// Yingge Can Run the code!
 @SpringBootApplication
 public class Team10Application {
 
