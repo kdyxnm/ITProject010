af3d2ae8d2ce62cfce8199d491b6b5c7d8403116
YucchenCAO01 <clark1320681142@gmail.com>
2022-09-20 19:52:43 +0800

    [feat] dashboard auto fit to phone screen

--- medione/target/classes/medioneVUE/medicine-one/vue.config.js
+++ medione/target/classes/medioneVUE/medicine-one/vue.config.js
@@ -15,6 +15,18 @@ module.exports = defineConfig({
         }),
     ],
   },
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http://localhost:8081',
+  //       ws: true,
+  //       changeOrigin: true,
+  //       pathRewrite: {
+  //         '/api': ''
+  //       }
+  //     }
+  //   }
+  // }
   // publicPath:'./'
 })
 
