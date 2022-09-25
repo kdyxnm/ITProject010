997375bf719ec7c3643fcd88343df25a73202cbc
UncleMcDonald <zisu1@student.unimelb.edu.au>
2022-09-25 19:19:52 +0800

    [chore] reconstruct project for simplifying deployment

---
+++ src/main/resources/medioneVUE/medicine-one/src/api/path.js
@@ -0,0 +1,28 @@
+//const port = process.env.PORT || 8081
+
+const base = {
+  //baseUrl     : 'http://localhost:' + port,
+  baseUrl     : "https://medione.herokuapp.com",
+  login       : '/user/login',
+  email       : '/mail/send',
+  register    : '/user/register',
+  dashboard   : '/dashboard',
+
+  pagination  : '/medicine/page',
+
+  search      : '/medicine/search/',
+
+  getMedicine : '/medicine/',
+
+
+  addLocation : '',
+  delLocation : '',
+
+  addMedicine : '',
+  delMedicine : '',
+  modMedicine : '',
+  
+  serch : ''
+}
+
+export default base;
---
+++ src/main/resources/medioneVUE/medicine-one/src/components/EditNote.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="overall_content">
+    <!-- <el-skeleton :rows="3" animated /> -->
+
+    <div style="margin: 20px 0" class="input_area">
+      <el-input
+        v-model="userNote"
+        maxlength="80"
+        placeholder="You can leave a message here"
+        show-word-limit
+        type="textarea"
+      />
+      
+      <div class="submit_button">
+        <input type="button" class="submitNote_button" value="submit" @click="submit_note">
+      </div>
+  </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+
+    }
+  },
+  props : {
+    userNote : {
+      type : String,
+      default : ''
+    },
+  }
+
+
+}
+
+</script>
+
+<style scoped>
+/* phone size */
+@media screen and (max-width: 992px){   
+  overall_content{
+    width: 100%;
+    padding: 2em;
+  }
+
+  .el-id-4110-2 {
+    min-height: 100px;
+  }
+
+  .submit_button {
+    margin-left: 15em;
+  }
+
+  .submitNote_button {
+    width: 7em;
+    height: 1.5em;
+    background: #E5E9FD;
+    color: #6E78F7;
+    border-style: none;
+    border-radius: 0.2em;
+    -webkit-transition-duration: 0.5s; /* Safari */
+    transition-duration: 0.5s;
+  }
+}
+
+</style>
